386dc9ed891c33b2f0242a8f3e3d4f082d254a0b
Nick Monaco <nick.monaco15@gmail.com>
2019-01-24 11:25:56 -0500

    add babel optional chaining

---
+++ config-overrides.js
@@ -0,0 +1,7 @@
+const { InjectBabelPlugin } = require('react-app-rewuired');
+
+// Override create-react-app settings without ejecting
+
+module.export = function override(config, env) {
+    return InjectBabelPlugin('@babel/plugin-proposal-optional-chaining', config);
+};
