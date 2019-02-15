c86ac37608b80ec6185617b3f0fdfb3ac6981374
monaco89 <nick.monaco15@gmail.com>
2019-02-15 18:30:11 -0500

    user accounts init files

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignOutButton from '../SignOut';
 
 import * as routes from '../constants/routes';
 
 import "./Navigation.css";
 
 // TODO 'Search' button
-const Navigation = () => (
+const Navigation = ({ session }) => (
+    <div>
+        {session && session.me ? (
+            <NavigationAuth session={session} />
+        ) : (
+                <NavigationNonAuth />
+            )}
+    </div>
+);
+
+const NavigationAuth = ({ session }) => (
     <div className="navbar">
         <Link to={routes.LANDING}><span role="img" aria-label="fire">🔥 </span></Link>
+        <Link to={routes.ACCOUNT}>{session.me.username}</Link>
+        <SignOutButton />
+    </div>
+)
+
+const NavigationNonAuth = () => (
+    <div className="navbar">
+        <Link to={routes.LANDING}><span role="img" aria-label="fire">🔥 </span></Link>
+        <Link to={routes.SIGN_IN}>Sign In</Link>
     </div>
 );
 
