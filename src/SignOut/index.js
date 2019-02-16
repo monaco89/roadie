2e71002596b0df84366a259e04ca7ddcb5eafb5e
monaco89 <nick.monaco15@gmail.com>
2019-02-15 21:19:18 -0500

    signout button

---
+++ src/SignOut/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ApolloConsumer } from 'react-apollo';
+
+import * as routes from '../constants/routes';
+import history from '../history';
+
+import Button from '@material-ui/core/Button';
+
+const signOut = client => {
+    localStorage.removeItem('token');
+    client.resetStore();
+    history.push(routes.SIGN_IN);
+}
+
+const SignOutButton = () => (
+    <ApolloConsumer>
+        {client => (
+            <Button
+                variant="contained"
+                color="primary"
+                type="button"
+                onClick={() => signOut(client)}
+            >
+                Logout
+        </Button>
+        )}
+    </ApolloConsumer>
+);
+
+export { signOut };
+
+export default SignOutButton;
