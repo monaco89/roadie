11f99253a328bb1de703350dfa6032471674c575
monaco89 <nick.monaco15@gmail.com>
2019-03-25 12:17:50 -0400

    update react

--- src/Map/event-info.js
+++ src/Map/event-info.js
@@ -1,21 +1,26 @@
-import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
-import moment from 'moment';
+import React, { PureComponent } from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
 
-import * as routes from '../constants/routes';
+import * as routes from "../constants/routes";
 
+// TODO Remove PureComponent
 export default class EventInfo extends PureComponent {
-    render() {
-        const { info } = this.props;
-        const displayName = `${info.venue.name}, ${info.venue.city.name}, ${info.venue.city.state}`;
-        const displayDate = moment(info.eventDate, "DD-MM-YYYY").format("MMM DD YYYY");
+  render() {
+    const { info } = this.props;
+    const displayName = `${info.venue.name}, ${info.venue.city.name}, ${
+      info.venue.city.state
+    }`;
+    const displayDate = moment(info.eventDate, "DD-MM-YYYY").format(
+      "MMM DD YYYY"
+    );
 
-        return (
-            <Link to={`${routes.EVENT}/${info.id}`}>
-                <h2>{displayDate}</h2>
-                <h3>{info.tour.name}</h3>
-                <span>{displayName}</span>
-            </Link>
-        );
-    }
+    return (
+      <Link to={`${routes.EVENT}/${info.id}`}>
+        <h2>{displayDate}</h2>
+        <h3>{info.tour.name}</h3>
+        <span>{displayName}</span>
+      </Link>
+    );
+  }
 }
