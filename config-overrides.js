e5dad2d4ad8ba77f2cb4cfa837b2478196a2fc09
monaco89 <nick.monaco15@gmail.com>
2019-03-30 14:08:16 -0400

    use craco, update dependencies

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { InjectBabelPlugin } = require('react-app-rewired');
-
-// Override create-react-app settings without ejecting
-
-module.export = function override(config, env) {
-    return InjectBabelPlugin('@babel/plugin-proposal-optional-chaining', config);
-};
---
+++ craco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  babel: {
+    plugins: [
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator"
+    ]
+  }
+};
